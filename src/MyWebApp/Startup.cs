388854324ce0b6562b1f571a1bb2bb1e2cc7c1a8
Vinicius Correa <vinicius.correa@thomsonreuters.com>
2020-11-14 10:26:59 -0300

    Health check endpoint

--- src/MyWebApp/Startup.cs
+++ src/MyWebApp/Startup.cs
@@ -26,6 +26,7 @@ namespace MyWebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddHealthChecks();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +46,7 @@ namespace MyWebApp
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHealthChecks("/healthcheck");
             });
         }
     }
